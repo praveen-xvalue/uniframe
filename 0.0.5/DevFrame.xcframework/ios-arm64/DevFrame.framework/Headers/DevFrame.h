14b52e3831954da512a7cad4c8037f5ed1bd57a7
apraveen_iOS <apraveen@xvaluetech.com>
2021-11-29 19:02:56 +0530

    0.0.5

---
+++ 0.0.5/DevFrame.xcframework/ios-arm64/DevFrame.framework/Headers/DevFrame.h
@@ -0,0 +1,18 @@
+//
+//  DevFrame.h
+//  DevFrame
+//
+//  Created by john j on 09/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DevFrame.
+FOUNDATION_EXPORT double DevFrameVersionNumber;
+
+//! Project version string for DevFrame.
+FOUNDATION_EXPORT const unsigned char DevFrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DevFrame/PublicHeader.h>
+
+
